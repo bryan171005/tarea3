d276ae29937b104243b162766a7fb582ac53f0ad
Bemc54 <bemc54@gmail.com>
2025-02-19 08:47:19 -0600

    se modifico el calculo de fecha y en frm varios se muestra fecha , sexo y estado

---
+++ tarea3/frmvarios.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MiPrimerApp2025C
+{
+    partial class frmvarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dtpNacimiento = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            dtpcalculo = new DateTimePicker();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dtpNacimiento
+            // 
+            dtpNacimiento.Location = new Point(3, 27);
+            dtpNacimiento.Name = "dtpNacimiento";
+            dtpNacimiento.Size = new Size(231, 23);
+            dtpNacimiento.TabIndex = 0;
+            dtpNacimiento.ValueChanged += dtpNacimiento_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 15);
+            label1.TabIndex = 1;
+            label1.Text = "fecha de nacimiento";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 15);
+            label2.TabIndex = 3;
+            label2.Text = "fecha de calculo";
+            // 
+            // dtpcalculo
+            // 
+            dtpcalculo.Location = new Point(3, 105);
+            dtpcalculo.Name = "dtpcalculo";
+            dtpcalculo.Size = new Size(231, 23);
+            dtpcalculo.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(dtpNacimiento, 0, 1);
+            tableLayoutPanel1.Controls.Add(label2, 0, 2);
+            tableLayoutPanel1.Controls.Add(dtpcalculo, 0, 3);
+            tableLayoutPanel1.Controls.Add(button1, 1, 4);
+            tableLayoutPanel1.Location = new Point(88, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.80282F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 66.19718F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 31F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 52F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 73F));
+            tableLayoutPanel1.Size = new Size(615, 228);
+            tableLayoutPanel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(310, 157);
+            button1.Name = "button1";
+            button1.Size = new Size(302, 41);
+            button1.TabIndex = 4;
+            button1.Text = "calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(88, 349);
+            button2.Name = "button2";
+            button2.Size = new Size(156, 23);
+            button2.TabIndex = 5;
+            button2.Text = "MOSTRAR DATOS";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 301);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(91, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 15);
+            label3.TabIndex = 7;
+            label3.Text = "CURP";
+            // 
+            // frmvarios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(tableLayoutPanel1);
+            Name = "frmvarios";
+            Text = "frmvarios";
+            Load += frmvarios_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dtpNacimiento;
+        private Label label1;
+        private Label label2;
+        private DateTimePicker dtpcalculo;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label3;
+    }
+}
---
+++ tarea3/frmvarios.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerApp2025C
+{
+    public partial class frmvarios : Form 
+    {
+        public frmvarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmvarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime nacimiento = dtpNacimiento.Value;
+            DateTime calculo = dtpcalculo.Value;
+
+            int edad = calculo.Year - nacimiento.Year;
+            int mes = calculo.Month - nacimiento.Month;
+            int dia = calculo.Day - nacimiento.Day;
+
+            if (mes < 0 || (mes == 0 && dia < 0))
+            {
+                edad--;
+                mes += 12; 
+            }
+
+           
+            if (dia < 0)
+            {
+                mes--; 
+                dia += DateTime.DaysInMonth(calculo.Year, calculo.Month - 1); 
+            }
+            MessageBox.Show("años" + edad + "meses" + mes + "dias" + dia, "resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void dtpNacimiento_ValueChanged(object sender, EventArgs e)
+        {
+            dtpcalculo.Enabled = true;
+            dtpcalculo.MinDate = dtpNacimiento.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fecha = textBox1.Text.Substring(4, 6);
+            String sexo = textBox1.Text.Substring(10, 1);
+            String estadoN= textBox1.Text.Substring(11,2);
+            string estadoNombre = "";
+
+            switch (estadoN)
+            {
+                case "AS": estadoNombre = "Aguascalientes"; break;
+                case "BC": estadoNombre = "Baja California"; break;
+                case "BS": estadoNombre = "Baja California Sur"; break;
+                case "CC": estadoNombre = "Campeche"; break;
+                case "CS": estadoNombre = "Chiapas"; break;
+                case "CH": estadoNombre = "Chihuahua"; break;
+                case "CL": estadoNombre = "Coahuila"; break;
+                case "CM": estadoNombre = "Colima"; break;
+                case "DF": estadoNombre = "Ciudad de México"; break;
+                case "DG": estadoNombre = "Durango"; break;
+                case "GT": estadoNombre = "Guanajuato"; break;
+                case "GR": estadoNombre = "Guerrero"; break;
+                case "HG": estadoNombre = "Hidalgo"; break;
+                case "JC": estadoNombre = "Jalisco"; break;
+                case "MC": estadoNombre = "México"; break;
+                case "MN": estadoNombre = "Michoacán"; break;
+                case "MS": estadoNombre = "Morelos"; break;
+                case "NT": estadoNombre = "Nayarit"; break;
+                case "NL": estadoNombre = "Nuevo León"; break;
+                case "OC": estadoNombre = "Oaxaca"; break;
+                case "PL": estadoNombre = "Puebla"; break;
+                case "QT": estadoNombre = "Querétaro"; break;
+                case "QR": estadoNombre = "Quintana Roo"; break;
+                case "SP": estadoNombre = "San Luis Potosí"; break;
+                case "SL": estadoNombre = "Sinaloa"; break;
+                case "SR": estadoNombre = "Sonora"; break;
+                case "TC": estadoNombre = "Tabasco"; break;
+                case "TS": estadoNombre = "Tamaulipas"; break;
+                case "TL": estadoNombre = "Tlaxcala"; break;
+                case "VZ": estadoNombre = "Veracruz"; break;
+                case "YN": estadoNombre = "Yucatán"; break;
+                case "ZS": estadoNombre = "Zacatecas"; break;
+                case "NE": estadoNombre = "Nacido en el extranjero"; break;
+                default: estadoNombre = "Estado no reconocido"; break;
+            }
+            string genero = "";
+
+            switch (sexo)
+            {
+                case "H": genero = "Hombre"; break;
+                case "M": genero = "Mujer"; break;
+                default: genero = "No identificado"; break;
+            }
+
+            MessageBox.Show("Fecha de nacimiento"+fecha+"Estado de nacimiento: "+estadoNombre+"Sexo"+genero);
+        }
+
+    }
+}
